684464a0591e6ce2bffd914d1d6ce47b81a95f08
oekshido <joseugarte2262005@gmail.com>
2020-03-04 16:15:33 -0300

    Settings now persist visually

--- static/js/settings.js
+++ static/js/settings.js
@@ -5,11 +5,22 @@ function getSettings(){
         else settings[e.id] = e.value
     }
     for(e of document.getElementsByTagName("select")){
-        if(e.getAttribute("type") == "number") settings[e.id] = e.value
+        if(e.getAttribute("type") == "number") settings[e.id] = Number(e.value)
         else settings[e.id] = e.value
     }
     return settings
 }
+
+function setSettings(settings){
+    for(e of document.getElementsByTagName("input")){
+        if(Object.keys(settings).includes(e.id)) e.value = settings[e.id]
+    }
+    for(e of document.getElementsByTagName("select")){
+        if(Object.keys(settings).includes(e.id)) e.value = settings[e.id]
+    }
+}
+
+// electron
 
 electron = require("electron")
 const Store = require('electron-store');
@@ -21,3 +32,8 @@ window.addEventListener("click", (e)=>{
         store.set("settings", JSON.stringify(getSettings()))
     }
 })
+
+window.onload = ()=>{
+    var settings = JSON.parse(store.get("settings") || "{}")
+    setSettings(settings)
+}

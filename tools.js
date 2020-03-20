1a6f2ee21d3be05e35a9b6d70b05ae43d582d068
oekshido <joseugarte2262005@gmail.com>
2020-03-20 19:57:44 -0300

    Fixed shifted matches bug

--- tools.js
+++ tools.js
@@ -2,18 +2,52 @@ function textToHTML(text){
     return `<p>${text}</p>`.replace(new RegExp("\t","gi"), " ").replace(new RegExp("\n","gi"), "</p><p>").replace(new RegExp("<p>[\n ]*</p>", "gi"), "")
 }
 
-function sourceToHTML(source, originalText, {minScore = 5}={}){
-    comparedText = source.text
-    originalTextCopy = originalText
-    for(var match of source.matches){
-        if(match.score >= minScore){
-            originalText = originalText.replace(originalText.slice(match.inputStart, match.inputEnd), `<u>${originalText.slice(match.inputStart, match.inputEnd)}</u>`)
-            comparedText = comparedText.replace(comparedText.slice(match.comparedStart, match.comparedEnd), `<u>${comparedText.slice(match.comparedStart, match.comparedEnd)}</u>`)
-        }
-    }
-    if(originalText == originalTextCopy){     // No matches greater than the minimum score or no matches at all
+function rangeOverlap(start1, end1, start2, end2){
+    if(end1 < start2 || end2 < start1){
         return false
     }
+    return [Math.min(start1, start2), Math.max(end1, end2)]
+}
+
+function addToRanges(ranges, start, end){
+    for(var i = 0; i < ranges.length; i++){
+        if(rangeOverlap(start, end, ranges[i][0], ranges[i][1])){
+            var newRange = rangeOverlap(start, end, ranges[i][0], ranges[i][1])
+            ranges.splice(i,1)
+            return addToRanges(ranges, newRange[0], newRange[1])
+        }
+    }
+    ranges.push([start, end])
+    return ranges
+}
+
+function sourceToHTML(source, originalText, {minScore = 5}={}){
+    var comparedText = source.text
+    var comparedTextCopy = comparedText
+    var originalTextCopy = originalText
+
+    var originalTextMatches = []
+    var comparedTextMatches = []
+
+    for(var match of source.matches){
+        if(match.score >= minScore){
+            originalTextMatches = addToRanges(originalTextMatches, match.inputStart, match.inputEnd)
+            comparedTextMatches = addToRanges(comparedTextMatches, match.comparedStart, match.comparedEnd)
+        }
+    }
+
+    for(var match of originalTextMatches){
+        originalText = originalText.replace(originalTextCopy.slice(match[0], match[1]), `<u>${originalTextCopy.slice(match[0], match[1])}</u>`)
+    }
+
+    for(var match of comparedTextMatches){
+        comparedText = comparedText.replace(comparedTextCopy.slice(match[0], match[1]), `<u>${comparedTextCopy.slice(match[0], match[1])}</u>`)
+    }
+
+    if(originalText == originalTextCopy){     // No matches greater than the minimum score or there are no matches at all
+        return false
+    }
+
     originalText = `<p>${originalText}</p>`.replace(new RegExp("\t","gi"), " ").replace(new RegExp("\n","gi"), "</p><p>").replace(new RegExp("<p>[\n ]*</p>", "gi"), "")
     comparedText = `<p>${comparedText}</p>`.replace(new RegExp("\t","gi"), " ").replace(new RegExp("\n","gi"), "</p><p>").replace(new RegExp("<p>[\n ]*</p>", "gi"), "")
     return [source.source, originalText, comparedText]
